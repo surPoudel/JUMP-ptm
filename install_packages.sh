688bdbee3beae2930d6595f6d0a61a43c50e47cb
Suresh Poudel <suresh.poudel@stjude.org>
2022-04-26 11:18:03 -0500

    conda install openpyxl added

---
+++ install_packages.sh
@@ -0,0 +1,12 @@
+conda install pyteomics -y
+conda install -c conda-forge biopython -y
+conda install -c anaconda seaborn -y
+conda install -c anaconda openpyxl -y
+conda install -c bioconda perl=5.26.2 -y
+conda install -c bioconda perl-parallel-forkmanager=2.02 -y
+conda install perl-statistics-r -y
+conda install -c bioconda perl-clone=0.42 -y
+conda install -c bioconda perl-excel-writer-xlsx=1.00 -y
+conda install -c conda-forge r-base=3.5.1 -y
+conda install -c r r-mass=7.3_51.5 -y
+
