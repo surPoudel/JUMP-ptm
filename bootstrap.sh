b697485deb8f8c75e4c947eb682881daa4ca71e7
surPoudel <72280240+surPoudel@users.noreply.github.com>
2022-05-05 08:57:18 -0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,5 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
         conda env create --prefix ./JUMPptm -f $PWD/jump_ptm_mac.yml
 fi
 
-conda env create --prefix ./JUMPptm -f $PWD/jump_ptm.yml
 $PWD/JUMPptm/bin/cpanm HTTP::Message~"<= 6.20" File::Copy File::Basename Scalar::Util LWP::UserAgent Set::Partition Sys::Hostname Spreadsheet::XLSX Statistics::Distributions
 conda activate $PWD/JUMPptm
